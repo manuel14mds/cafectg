01feeea2e9418a1e5f2d66e4dc2ec294bcc67350
manuel14mds <manuel14mds@gmail.com>
2022-11-03 10:41:26 -0500

    product detail added

--- src/dao/mongoDAO/MongoContainer.js
+++ src/dao/mongoDAO/MongoContainer.js
@@ -15,7 +15,7 @@ export default class MongoContainer {
     }
 
     getById = async (id) => {
-        let result = await this.modelService.findById(id)//cuidado con esta prueba, puede dar error, no testeada
+        let result = await this.modelService.findById(id).lean()//cuidado con esta prueba, puede dar error, no testeada
         //let result = await this.modelService.findOne({_id:id}) 
         return result
     }
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -11,23 +11,37 @@ router.get('/', (req,res)=>{
     const user = jwt.verify(token, config.jwt.SECRET)
     res.render('index', {user})
 })
+
 router.get('/login', (req,res)=>{
     res.render('login')
 })
+
 router.get('/register', (req,res)=>{
     res.render('register')
 })
+
+router.get('/productDetail/:pid', async(req,res)=>{
+    console.log('id:', req.params.pid)
+    try {
+        let product = await services.ProductService.getById(req.params.pid)
+        res.render('detail',{product})
+        
+    } catch (error) {
+        res.status(500).send('insternal error')
+    }
+})
+
 router.get('/account', (req,res)=>{
     const token = req.cookies[config.jwt.COOKIE]
     if(!token) return res.render('account', {user:false})
     const user = jwt.verify(token, config.jwt.SECRET)
     res.render('account',{user})
 })
+
 router.get('/category', async(req,res)=>{
     const ctg = req.query.category
     if(ctg === 'all'){
         let products = await services.ProductService.getAll()
-        console.log(products)
         res.render('category',{products})
     }else{
         let products = await services.ProductService.findByCategory(ctg)
